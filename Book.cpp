441f20c52aeb10409b885e2b2c9db85006b70fc7
waynon <57490099+waynon@users.noreply.github.com>
2019-11-24 15:27:19 +0800

    Add files via upload

---
+++ Book.cpp
@@ -0,0 +1,67 @@
+#include "Book.h"	 /*****请仿照Course并参照数据库，实现书籍(部)表的增删改查DAO操作*****/
+
+void BookDAOImpl::InsertBook(Book book)
+{
+	CDaoDatabase* conn = Getconnection();
+	CString sql;
+	sql.Format("insert into pkbook values('%s','%s','%s','%s','%s','%s','%s','%s')",
+		book.getName().c_str(), book.getTopic().c_str(), book.getIsbnAndPrice().c_str(),
+		book.getPublishInfo().c_str(), book.getCnLibClass().c_str(), 
+		book.getPersonInCharge().c_str(), book.getSubPersonInCharge().c_str(),
+		book.getCarrierShapeItem().c_str());
+	conn->Execute(sql, dbSeeChanges);
+	conn->Close();
+	delete conn;
+}
+
+void BookDAOImpl::DeleteBook(string id)
+{
+	CDaoDatabase* conn = Getconnection();
+	CString sql;
+	sql.Format("delete from pkbook where isbnprice = '%s'", id.c_str());
+	conn->Execute(sql, dbSeeChanges);
+	conn->Close();
+	delete conn;
+}
+
+void BookDAOImpl::UpdateBook(string id, string field, string value)
+{
+	CDaoDatabase* conn = Getconnection();
+	CString sql;
+	sql.Format("update pkbook set %s = '%s' where isbnprice = '%s'", field.c_str(), 
+		value.c_str(), id.c_str());
+	conn->Execute(sql, dbSeeChanges);
+	conn->Close();
+	delete conn;
+}
+
+Book BookDAOImpl::SearchBook(string id)
+{
+	CDaoDatabase* conn = Getconnection();
+	CDaoRecordset rs(conn);
+	Book book;
+	CString sql;
+	sql.Format("select * from pkbook where isbnprice= '%s'", id.c_str());
+	rs.Open(AFX_DAO_USE_DEFAULT_TYPE, sql, dbSeeChanges);
+	COleVariant value;
+	rs.GetFieldValue("bn", value);
+	book.setName((LPCTSTR)value.pbstrVal);
+	rs.GetFieldValue("stheme", value);
+	book.setTopic((LPCTSTR)value.pbstrVal);
+	rs.GetFieldValue("isbnprice", value);
+	book.setIsbnAndPrice((LPCTSTR)value.pbstrVal);
+	rs.GetFieldValue("pubitem", value);
+	book.setPublishInfo((LPCTSTR)value.pbstrVal);
+	rs.GetFieldValue("cpknum", value);
+	book.setCnLibClass((LPCTSTR)value.pbstrVal);
+	rs.GetFieldValue("privp", value);
+	book.setPersonInCharge((LPCTSTR)value.pbstrVal);
+	rs.GetFieldValue("privnp", value);
+	book.setSubPersonInCharge((LPCTSTR)value.pbstrVal);
+	rs.GetFieldValue("formitem", value);
+	book.setCarrierShapeItem((LPCTSTR)value.pbstrVal);
+	conn->Close();
+	rs.Close();
+	delete conn;
+	return book;
+}
