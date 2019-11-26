d745c62552e0d943fa8ee91043613194e5887d54
bxy0125 <58174583+bxy0125@users.noreply.github.com>
2019-11-26 23:26:20 +0800

    Add files via upload

--- Comment.h
+++ Comment.h
@@ -2,15 +2,49 @@
 
 class Comment
 {
-
+private:
+	string ISBNprice;
+	string com;
+	string cnum;
+public:
+	string getISBNprice()
+	{
+		return ISBNprice;
+	}
+	string getCom()
+	{
+		return com;
+	}
+	string getCnum()
+	{
+		return cnum;
+	}
+	void setISBNprice(string ISBNprice)
+	{
+		this->ISBNprice = ISBNprice;
+	}
+	void setCom(string com)
+	{
+		this->com = com;
+	}
+	void setCnum(string cnum)
+	{
+		this->cnum = cnum;
+	}
 };
 
 class CommentDAO
 {
-
+	virtual void InsertComment(Comment comment) = 0;
+	virtual void DeleteComment(string ISBNprice, string cnum) = 0;
+	virtual void UpdateComment(string ISBNprice, string cnum, string field, string value) = 0;
+	virtual vector<Comment> SearchComment(string ISBNprice, string cnum) = 0;
 };
 
 class CommentDAOImpl : public CommentDAO, public DAObase
 {
-
+	void InsertComment(Comment comment) = 0;
+	void DeleteComment(string ISBNprice, string cnum) = 0;
+	void UpdateComment(string ISBNprice, string cnum, string field, string value) = 0;
+	vector<Comment> SearchComment(string ISBNprice, string cnum) = 0;
 };
