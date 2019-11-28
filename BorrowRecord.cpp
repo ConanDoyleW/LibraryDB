54dda409051f9a855751868be2e3d5b73af16dcd
ConanDoyleW <57653243+ConanDoyleW@users.noreply.github.com>
2019-11-28 23:27:46 +0800

    Update BorrowRecord.cpp

--- BorrowRecord.cpp
+++ BorrowRecord.cpp
@@ -1,12 +1,10 @@
-#include "BorrowRecord.h"	/*****�����Course���������ݿ⣬ʵ�ֽ��ı�����ɾ�Ĳ�DAO����*****/
+#include "BorrowRecord.h"	/*****请仿照Course并参照数据库，实现借阅表的增删改查DAO操作*****/
+
 void BorrowRecordDAOImpl::InsertBorrowRecord(BorrowRecord borrowrecord)
 {
 	CDaoDatabase* conn = Getconnection();
 	CString sql;
-	sql.Format("insert into BOOKB(CNUM,BARCODE,BTIME,RTIME) values('%s','%s','%s','%s')",
-		borrowrecord.getCnum().c_str(), borrowrecord.getBarcode().c_str(),
-		borrowrecord.getBtime().GetYear(),borrowrecord.getBtime().GetMonth(),borrowrecord.getBtime().GetDay(),
-		borrowrecord.getRtime().GetYear(), borrowrecord.getRtime().GetMonth(), borrowrecord.getRtime().GetDay());
+	sql.Format("insert into BOOKB(CNUM,BARCODE,BTIME,RTIME) values('%s','%s','%s','%s')", borrowrecord.getCnum().c_str(), borrowrecord.getBarcode().c_str(), borrowrecord.getBtime().c_str(), borrowrecord.getRtime().c_str());
 	conn->Execute(sql, dbSeeChanges);
 	conn->Close();
 	delete conn;
@@ -33,28 +31,39 @@ void BorrowRecordDAOImpl::UpdateBorrowRecord(string cnum, string barcode, string
 	delete conn;
 }
 
-vector<BorrowRecord> BorrowRecordDAOImpl::SearchBorrowRecord(string cnum, string barcode)
+vector<BorrowRecord> BorrowRecordDAOImpl::SearchBorrowRecord(string par, string mode)
 {
 	CDaoDatabase* conn = Getconnection();
 	CDaoRecordset rs(conn);
-	BorrowRecord borrowrecord;
 	CString sql;
-	sql.Format("select * from BOOKB where CNUM = '%s' and BARCODE = '%s'", cnum.c_str(), barcode.c_str());
-	vector<BorrowRecord> v = vector<BorrowRecord>();
+	sql.Format("select * from BOOKB where %s = '%s' ", mode.c_str(), par.c_str());;
+	vector<BorrowRecord> v;
 	rs.Open(AFX_DAO_USE_DEFAULT_TYPE, sql, dbSeeChanges);
-	COleVariant value;
-	rs.GetFieldValue("CNUM", value);
-	borrowrecord.setCnum((LPCTSTR)value.pbstrVal);
-	rs.GetFieldValue("BARCODE", value);
-	borrowrecord.setBarcode((LPCTSTR)value.pbstrVal);
-	rs.GetFieldValue("BTIME", value);
-	borrowrecord.setBtime((LPCTSTR)value);
-	rs.GetFieldValue("RTIME", value);
-	borrowrecord.setRtime((LPCTSTR)value);
-	v.push_back(comment);
-	rs.MoveNext();
+	while (!rs.IsEOF())
+	{
+		BorrowRecord borrowrecord;
+		COleVariant value;
+		rs.GetFieldValue("CNUM", value);
+		borrowrecord.setCnum((LPCTSTR)value.pbstrVal);
+		rs.GetFieldValue("BARCODE", value);
+		borrowrecord.setBarcode((LPCTSTR)value.pbstrVal);
+		rs.GetFieldValue("BTIME", value);
+		borrowrecord.setBtime((LPCTSTR)value.pbstrVal);
+		rs.GetFieldValue("RTIME", value);
+		borrowrecord.setRtime((LPCTSTR)value.pbstrVal);
+		v.push_back(borrowrecord);
+		rs.MoveNext();
+	}
 	conn->Close();
 	rs.Close();
 	delete conn;
 	return v;
+}
+
+BorrowRecord::BorrowRecord(string cnu, string bar, string bti, string rti)
+{
+	cnum = cnu;
+	barcode = bar;
+	btime = bti;
+	rtime = rti;
 }
