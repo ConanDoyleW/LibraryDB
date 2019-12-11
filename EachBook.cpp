ca3951df45f7a0a5d34426c166bead4d36924501
ConanDoyleW <57653243+ConanDoyleW@users.noreply.github.com>
2019-12-11 12:47:03 +0800

    Update EachBook.cpp

--- EachBook.cpp
+++ EachBook.cpp
@@ -5,9 +5,9 @@ void EachBookDAOImpl::InsertBook(EachBook book)
 	CDaoDatabase* conn = Getconnection();
 	CString sql;
 	sql.Format("insert into pbook values('%s','%s','%s','%s','%s','%s')",
-		book.getIndexNum().c_str,book.getIsbnAndPrice().c_str(),
-		book.getBarCode().c_str(),book.getVolume().c_str(),
-		book.getLocation().c_str(),book.getStatus().c_str());
+		book.getIndexNum().c_str, book.getIsbn().c_str(),
+		book.getBarCode().c_str(), book.getVolume().c_str(),
+		book.getLocation().c_str(), book.getStatus().c_str());
 	conn->Execute(sql, dbSeeChanges);
 	conn->Close();
 	delete conn;
@@ -34,21 +34,21 @@ void EachBookDAOImpl::UpdateBook(string id, string field, string value)
 	delete conn;
 }
 
-vector<EachBook> EachBookDAOImpl::SearchBook(string id)
+vector<EachBook> EachBookDAOImpl::SearchBook(string par, string mode)
 {
 	CDaoDatabase* conn = Getconnection();
 	CDaoRecordset rs(conn);
-	EachBook book;
 	CString sql;
-	sql.Format("select * from pbook where barcode= '%s'", id.c_str());
+	sql.Format("select * from pbook where %s = '%s' ", mode.c_str(), par.c_str());
 	vector<EachBook> v = vector<EachBook>();
 	rs.Open(AFX_DAO_USE_DEFAULT_TYPE, sql, dbSeeChanges);
 	while (!rs.IsEOF())
 	{
+		EachBook book;
 		COleVariant value;
 		rs.GetFieldValue("callnum", value);
 		book.setIndexNum((LPCTSTR)value.pbstrVal);
-		rs.GetFieldValue("isbnprice", value);
+		rs.GetFieldValue("isbn", value);
 		book.setIsbnAndPrice((LPCTSTR)value.pbstrVal);
 		rs.GetFieldValue("barcode", value);
 		book.setBarCode((LPCTSTR)value.pbstrVal);
@@ -71,10 +71,10 @@ EachBook::EachBook()
 {
 }
 
-EachBook::EachBook(string indexNum, string isbnAndPrice, string barCode, string volume, string location, string status)
+EachBook::EachBook(string indexNum, string isbn, string barCode, string volume, string location, string status)
 {
 	this->indexNum = indexNum;
-	this->isbnAndPrice = isbnAndPrice;
+	this->isbn = isbn;
 	this->barCode = barCode;
 	this->volume = volume;
 	this->location = location;
