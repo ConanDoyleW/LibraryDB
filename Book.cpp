1a60a64a9a917945a6badbeff73961beee4bb973
ConanDoyleW <57653243+ConanDoyleW@users.noreply.github.com>
2019-12-11 12:41:49 +0800

    Update Book.cpp

--- Book.cpp
+++ Book.cpp
@@ -5,8 +5,8 @@ void BookDAOImpl::InsertBook(Book book)
 	CDaoDatabase* conn = Getconnection();
 	CString sql;
 	sql.Format("insert into pkbook values('%s','%s','%s','%s','%s','%s','%s','%s')",
-		book.getName().c_str(), book.getTopic().c_str(), book.getIsbnAndPrice().c_str(),
-		book.getPublishInfo().c_str(), book.getCnLibClass().c_str(), 
+		book.getName().c_str(), book.getTopic().c_str(), book.getIsbn().c_str(),
+		book.getPublishInfo().c_str(), book.getCnLibClass().c_str(),
 		book.getPersonInCharge().c_str(), book.getSubPersonInCharge().c_str(),
 		book.getCarrierShapeItem().c_str());
 	conn->Execute(sql, dbSeeChanges);
@@ -18,7 +18,7 @@ void BookDAOImpl::DeleteBook(string id)
 {
 	CDaoDatabase* conn = Getconnection();
 	CString sql;
-	sql.Format("delete from pkbook where isbnprice = '%s'", id.c_str());
+	sql.Format("delete from pkbook where isbn = '%s'", id.c_str());
 	conn->Execute(sql, dbSeeChanges);
 	conn->Close();
 	delete conn;
@@ -28,31 +28,31 @@ void BookDAOImpl::UpdateBook(string id, string field, string value)
 {
 	CDaoDatabase* conn = Getconnection();
 	CString sql;
-	sql.Format("update pkbook set %s = '%s' where isbnprice = '%s'", field.c_str(), 
+	sql.Format("update pkbook set %s = '%s' where isbn = '%s'", field.c_str(),
 		value.c_str(), id.c_str());
 	conn->Execute(sql, dbSeeChanges);
 	conn->Close();
 	delete conn;
 }
 
-vector<Book> BookDAOImpl::SearchBook(string id)
+vector<Book> BookDAOImpl::SearchBook(string par, string mode)
 {
 	CDaoDatabase* conn = Getconnection();
 	CDaoRecordset rs(conn);
-	Book book;
 	CString sql;
-	sql.Format("select * from pkbook where isbnprice= '%s'", id.c_str());
+	sql.Format("select * from pkbook where %s like'%%%s%%' ", mode.c_str(), par.c_str());
 	rs.Open(AFX_DAO_USE_DEFAULT_TYPE, sql, dbSeeChanges);
-	vector<Book> v = vector<Book>();
+	vector<Book> v;
 	while (!rs.IsEOF())
 	{
+		Book book;
 		COleVariant value;
 		rs.GetFieldValue("bn", value);
 		book.setName((LPCTSTR)value.pbstrVal);
 		rs.GetFieldValue("stheme", value);
 		book.setTopic((LPCTSTR)value.pbstrVal);
-		rs.GetFieldValue("isbnprice", value);
-		book.setIsbnAndPrice((LPCTSTR)value.pbstrVal);
+		rs.GetFieldValue("isbn", value);
+		book.setIsbn((LPCTSTR)value.pbstrVal);
 		rs.GetFieldValue("pubitem", value);
 		book.setPublishInfo((LPCTSTR)value.pbstrVal);
 		rs.GetFieldValue("cpknum", value);
@@ -76,12 +76,11 @@ Book::Book()
 {
 }
 
-Book::Book(string name, string topic, string isbnAndPrice, string indexNumber, string publishInfo, string cnLibClassm, string personInCharge, string subPersonInCharge, string carrierShapeItem)
+Book::Book(string name, string topic, string isbn, string publishInfo, string cnLibClass, string personInCharge, string subPersonInCharge, string carrierShapeItem)
 {
 	this->name = name;
 	this->topic = topic;
-	this->isbnAndPrice = isbnAndPrice;
-	this->indexNumber = indexNumber;
+	this->isbn = isbn;
 	this->publishInfo = publishInfo;
 	this->cnLibClass = cnLibClass;
 	this->personInCharge = personInCharge;
